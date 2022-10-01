838f5489abe8831491e373974a7a5bc7b49f33e0
gusntc <Natacha_rungbanpant@cmu.ac.th>
2022-10-01 17:23:09 +0700

    fix1

--- pages/api/room/[roomId]/message.js
+++ pages/api/room/[roomId]/message.js
@@ -8,66 +8,73 @@ import { checkToken } from "../../../../backendLibs/checkToken";
 export default function roomIdMessageRoute(req, res) {
   if (req.method === "GET") {
     //check token
-    const user = checkToken(req); //{ username: 'admin', isAdmin: true }
-    if (!user) {
+    const username = checkToken(req);
+    if (!username)
       return res.status(401).json({
         ok: false,
-        message: "Yon don't permission to access this api",
+        message: "You don't permission to access this api",
       });
-    }
+
     //get roomId from url
     const roomId = req.query.roomId;
 
     const rooms = readChatRoomsDB();
-    //console.log(rooms);
 
     //check if roomId exist
-    const foundRoomId = rooms.find((x) => roomId === x.roomId);
-    //console.log(foundRoomId);
-    //find room and return
-    if (!foundRoomId) {
-      //check null,false
+    const checkroomid = rooms.find((x) => x.roomId === roomId);
+
+    if (!checkroomid) {
       return res.status(404).json({ ok: false, message: "Invalid room id" });
     }
-    console.log(foundRoomId.messages);
-    return res.status(200).json({ ok: true, message: foundRoomId.messages });
 
+    //find room and return
     //...
+
+    return res.json({
+      ok: true,
+      messages: checkroomid.messages,
+    });
   } else if (req.method === "POST") {
-    const user = checkToken(req); //{ username: 'admin', isAdmin: true }
-    if (!user) {
+    //check token
+    const username = checkToken(req);
+    if (!username)
       return res.status(401).json({
         ok: false,
-        message: "Yon don't permission to access this api",
+        message: "You don't permission to access this api",
       });
-    }
-    //check token
 
     //get roomId from url
     const roomId = req.query.roomId;
     const rooms = readChatRoomsDB();
 
     //check if roomId exist
-    const foundRoomId = rooms.find((x) => roomId === x.roomId);
-    console.log(foundRoomId);
+    const checkroomid = rooms.find((x) => x.roomId === roomId);
 
+    if (!checkroomid) {
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
+    }
     //validate body
-    if (typeof req.body.text !== "string" || req.body.text.length === 0)
+    if (
+      typeof req.body.text !== "string" ||
+      req.body.text.length === 0 ||
+      req.body.text === undefined
+    )
       return res.status(400).json({ ok: false, message: "Invalid text input" });
 
     //create message
-    const newTodo = {
-      messageId: uuidv4(),
+    const newId = uuidv4();
+    const newText = {
+      messageId: newId,
       text: req.body.text,
-      username: user.username,
+      username: username.username,
     };
+    // console.log(newText);
 
-    const findIdx = rooms.findIndex((x) => x.roomId === roomId);
-
-    rooms[findIdx].messages.push(newTodo);
-
+    const newchat = rooms.find((x) => x.roomId === roomId).messages;
+    // console.log(rooms.find((x) => x.roomId === roomId).messages);
+    newchat.push(newText);
     writeChatRoomsDB(rooms);
 
-    return res.status(200).json({ ok: true, message: newTodo });
+    return res.json({ ok: true, message: newText });
   }
 }
